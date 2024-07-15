f1af27b51714f4fd4ecc4d101b78ea3cae096309
jotavetech <ojoaogugel@gmail.com>
2024-07-15 10:38:43 -0300

    feat: add cors config

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 const express = require("express");
+
 const path = require("path");
 
 require("dotenv").config();
 
+const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:3000";
 const PORT = process.env.PORT || 8000;
 
 const app = express();
@@ -12,7 +14,14 @@ const httpServer = createServer(app);
 
 app.use(express.static(path.join(__dirname, "public")));
 
-const io = new Server(httpServer);
+const io = new Server(httpServer, {
+  cors: {
+    origin: CLIENT_URL,
+    methods: ["GET", "POST"],
+    transports: ["websocket", "polling"],
+    credentials: true,
+  },
+});
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
