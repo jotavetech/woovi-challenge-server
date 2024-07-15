c6cb706109b7ed10841644eec52eef56bc1904dc
jotavetech <ojoaogugel@gmail.com>
2024-07-15 10:11:48 -0300

    feat: send html files and emit payment to websockets

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 const express = require("express");
+const path = require("path");
 
 require("dotenv").config();
 
@@ -9,7 +10,21 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 const httpServer = createServer(app);
 
+app.use(express.static(path.join(__dirname, "public")));
+
 const io = new Server(httpServer);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/payment/:id", (req, res) => {
+  const { id } = req.params;
+
+  io.emit("payment-success", id);
+
+  res.sendFile(path.join(__dirname, "payment.html"));
+});
 
 httpServer.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
